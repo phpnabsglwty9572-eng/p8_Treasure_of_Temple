5a8c6ed24ccc8ea251fa24603cddd5909c2acaf3
shawnDane <shawndane@qq.com>
2026-08-27 09:41:55 +0800

    update url

--- lib/game/privacy_policy_config.dart
+++ lib/game/privacy_policy_config.dart
@@ -3,5 +3,5 @@ class PrivacyPolicyConfig {
   PrivacyPolicyConfig._();
 
   /// 在这里填写隐私政策页面地址。
-  static const String url = 'https://docs.google.com/document/d/1UMz3kmv3fbchs-LwTiDjkiUZXscZtFchjYHROViUDRM/edit?usp=sharing';
+  static const String url = 'https://docs.google.com/document/d/1wMWtXlN6Lw_Bpy2Yy_z7N37uMiD6unHuxreByUCJHLI/edit?usp=sharing';
 }
